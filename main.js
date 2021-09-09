dcedfe0979ca124053bb04a199aedc24ebec94e3
codeuser-creator <85422572+codeuser-creator@users.noreply.github.com>
2021-09-08 21:09:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Ranbir";
+console.log(first_name);
+var last_name = "Kapoor";
+console.log(last_name);
+var A = 10;
+var B = 5;
+var add = a + b;
+console.log(add); 
+var full_name = first_name.concat(last_name);
+console.log(full_name);
